bf68de4272ce68d30af6b603a23ac3a593bc94c4
Weverton <wevertoncesar@gmail.com>
2021-08-31 21:33:21 -0300

    Adiciona roteamento e outras configurações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './navigation/menu/menu.component';
 import { HomeComponent } from './navigation/home/home.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { AboutComponent } from './institutional/about/about.component';
+import { ContactComponent } from './institutional/contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
